58fc42523268976f8b4ed0451ffab351ad949f6b
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-02-11 20:24:38 +0100

    Update index.tsx

--- app/src/components/common/logos/omen/index.tsx
+++ app/src/components/common/logos/omen/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  .logo {
+    font-size: 19px;
+    font-weight: 500;
+  }
+`
 
 export const OmenLogo = () => {
   return (
-    <Wrapper fontWeight="500" fontSize="18px">🧙‍♂️ Merlin</Wrapper>
+    <Wrapper>
+      <span className="logo">Merlin</span>
+    </Wrapper>
   )
 }
