da61e33ec4a7e3a036f9640c221763347722733a
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-02-12 13:21:49 +0100

    Update market_home.tsx

--- app/src/components/market/market_list/market_home.tsx
+++ app/src/components/market/market_list/market_home.tsx
@@ -111,7 +111,6 @@ const ListWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
   gap: 16px;
-  min-height: 355px;
 `
 
 const NoMarketsAvailable = styled.p`
