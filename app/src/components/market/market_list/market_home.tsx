f71342800952b3bee2b45257dea29d44db66494e
danielfetz <118893474+danielfetz@users.noreply.github.com>
2024-02-15 15:29:26 +0100

    Update market_home.tsx

--- app/src/components/market/market_list/market_home.tsx
+++ app/src/components/market/market_list/market_home.tsx
@@ -518,9 +518,7 @@ export const MarketHome: React.FC<Props> = (props: Props) => {
           }}
         >
           <ActionInfo>
-            <ActionInfoTitle>
-              How Merlin works
-            </ActionInfoTitle>
+            <ActionInfoTitle>How Merlin works</ActionInfoTitle>
           </ActionInfo>
         </InfoCard>  
         <InfoCard style={{ background: 'linear-gradient(to right, rgb(211, 84, 84) 0%, rgb(200, 137, 110) 100%)' }} />
